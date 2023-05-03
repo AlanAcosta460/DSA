384e1b786fce5ec1657ab2110b48518e5c5f727b
Alan Acosta <alanomar460@gmail.com>
2023-05-02 19:14:50 -0600

    T7 - cambio de nombre y comentarios

---
+++ T7-AcostaP-GutierrezG.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Nodo {
+    int dato;
+    struct Nodo *siguiente;
+} Nodo;
+
+Nodo *lista = NULL;
+
+void insertarLista(Nodo **lista, int n) { // Insertar un nodo a la lista
+    Nodo *nuevoNodo = (Nodo*) malloc(sizeof(Nodo));
+    nuevoNodo->dato = n;
+
+    Nodo *aux1 = *lista, *aux2 = NULL; 
+
+    while (aux1 != NULL && aux1->dato < n) { // Recorrer la lista hasta encontrar el lugar donde insertar el nuevo nodo
+        aux2 = aux1;
+        aux1 = aux1->siguiente;
+    }
+    
+    if(aux2 == NULL) *lista = nuevoNodo; // Primer elemento de la lista
+    else aux2->siguiente = nuevoNodo; // Elementos siguientes de la lista
+    
+    nuevoNodo->siguiente = aux1;
+
+    printf("\tElemento %d insertado a la lista\n\n", n);
+}
+
+void mostrarLista(Nodo *lista) { // Mostrar los elementos de la lista
+    Nodo *actual = lista;
+
+    if(actual != NULL) printf(".:Elementos de la lista:.\n");
+    else printf("Lista Vacia");
+
+    while (actual != NULL) { // Recorrer la lista
+        printf("%d \t", actual->dato);
+        actual = actual->siguiente;
+    }
+    printf("\n");
+}
+
+void buscarLista(Nodo *lista, int n) { // Buscar un elemento en la lista (función extra)
+    Nodo *actual = lista; 
+    bool flag = false;
+
+    while (actual != NULL && actual->dato <= n) { // Recorrer la lista
+        if(actual->dato == n) flag = true;
+        actual = actual->siguiente;
+    }
+    
+    if(flag == true) printf("\tSe encontro el elemento %d en la lista!!!\n", n);
+    else printf("\tNo se encontro el elemento %d en la lista!!!\n", n);
+}
+
+void eliminarNodo(Nodo **lista, int n) // Eliminar un nodo de la lista (función extra)
+{
+    if(*lista == NULL) {
+        printf("\nLista Vacia\n");
+        return;
+    }
+
+    Nodo *auxBorrar = *lista, *anterior = NULL;
+
+    while (auxBorrar != NULL && auxBorrar->dato != n) { // Recorrer la lista
+        anterior = auxBorrar;
+        auxBorrar = auxBorrar->siguiente;
+    }
+
+    if(auxBorrar == NULL) printf("\tElemento no encontrado\n"); 
+    else if(anterior == NULL) { //Borrar el primer elemento de la lista
+        *lista = (*lista)->siguiente;
+        free(auxBorrar);
+        printf("\tElemento borrado con exito!!!\n");
+    }
+    else { // Borrar un elemento de la lista
+        anterior->siguiente = auxBorrar->siguiente;
+        free(auxBorrar);
+        printf("\tElemento borrado con exito!!!\n");
+    }
+}
+
+int menu() { // Menu de opciones
+    int op;
+
+    printf("\t.::Menu::.\n");
+    printf("1. Insertar un numero a la lista\n");
+    printf("2. Mostrar los elementos de la lista\n");
+    printf("3. Buscar elemento en la lista\n");
+    printf("4. Eleminar elemento de la lista\n");
+    printf("5. Salir\n\n");
+    
+    do { // Validar la opcion ingresada
+        printf("Ingrese una opcion: ");
+        scanf("%d", &op);
+        printf("\n");
+    } while(op < 1 || op > 6);
+
+    return op;
+}
+
+int main() {
+    int dato, op;
+    
+    do {
+        op = menu();
+
+        switch(op) {
+            case 1:
+                printf("Ingrese un numero: ");
+                scanf("%d", &dato);
+                insertarLista(&lista, dato);
+                break;
+            
+            case 2:
+                mostrarLista(lista);
+                printf("\n");
+                break;
+
+            case 3:
+                printf("Ingrese el elemento a buscar: "); 
+                scanf("%d", &dato);
+                buscarLista(lista, dato);
+                printf("\n");
+                break; 
+
+            case 4:
+                printf("Ingrese el elemento a elminar: "); 
+                scanf("%d", &dato);
+                eliminarNodo(&lista, dato);
+                printf("\n");
+                break;
+
+            default:
+                printf("Gracias por usar el programa!!!");
+                break; 
+        }
+    } while(op != 5);
+
+    return 0;
+}
