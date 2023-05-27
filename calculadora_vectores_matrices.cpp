03d6df3c02f11fa4301ac265c3087c3136909ad3
Alan Acosta <alanomar460@gmail.com>
2023-05-27 13:32:55 -0600

    traza

--- calculadora_vectores_matrices.cpp
+++ calculadora_vectores_matrices.cpp
@@ -209,7 +209,7 @@ void traza() {
     sum = 0;
     for(int i = 0; i < rows; i ++) {
         for(int j = 0; j < cols; j ++) {
-            if(i == j) sum = mat1[i][j];
+            if(i == j) sum += mat1[i][j];
         }
     }
     cout<<"\n.: Resultado :.\n"<<sum;
